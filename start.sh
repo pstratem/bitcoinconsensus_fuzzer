d99593ca839467df68bf8abe887263fef07c8abf
Patrick Strateman <patrick.strateman@gmail.com>
2015-10-03 18:42:47 -0700

    Improvements

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ export AFL_SKIP_CPUFREQ=1
 
 mkdir outputs;
 
-while afl-gotcpu; do
-    nohup afl-fuzz -i corpus/ -o outputs -M fuzzer$i ./run_script @@ >/dev/null 2>/dev/null &
+nohup afl-fuzz -i corpus/ -o outputs -M fuzzer ./run_script @@ >/dev/null 2>/dev/null &
+for i in $(seq 1 $(nproc)); do
+    nohup afl-fuzz -i corpus/ -o outputs -S fuzzer$i ./run_script @@ >/dev/null 2>/dev/null &
 done;
