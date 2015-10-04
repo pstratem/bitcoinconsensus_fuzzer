5d28abd07b7a017681310ae58f39fd1ca4e77e6c
Patrick Strateman <patrick.strateman@gmail.com>
2015-10-03 18:53:53 -0700

    Derp

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ killall afl-fuzz;
 mkdir outputs;
 
 nohup afl-fuzz -i corpus/ -o outputs -M fuzzer ./run_script @@ >/dev/null 2>/dev/null &
-declate -i NPROC;
+declare -i NPROC;
 NPROC=$(nproc)*2
 for i in $(seq 1 $NPROC); do
     nohup afl-fuzz -i corpus/ -o outputs -S fuzzer$i ./run_script @@ >/dev/null 2>/dev/null &
