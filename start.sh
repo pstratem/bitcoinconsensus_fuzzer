2ae1c1689b21e2ad16ac70537282c5279f9655cb
Patrick Strateman <patrick.strateman@gmail.com>
2015-10-03 18:37:33 -0700

    improve startup scripts

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ export AFL_SKIP_CPUFREQ=1
 
 ./consolidate.sh
 
-for i in $(seq 1 $(nproc)); do
+mkdir outputs;
+
+while afl-gotcpu; do
     nohup afl-fuzz -i corpus/ -o outputs -M fuzzer$i ./run_script @@ >/dev/null 2>/dev/null &
 done;
